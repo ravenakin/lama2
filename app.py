22c340adc37ab27e99566bda88a09032749c5b49
ffreemt <yucongo+fmt@gmail.com>
2023-07-22 13:53:42 +0800

    Fix api (remove api_name=None)

--- app.py
+++ app.py
@@ -396,7 +396,7 @@ with gr.Blocks(
         outputs=[msg, chatbot],
         queue=True,
         show_progress="full",
-        api_name=None,
+        # api_name=None,
     ).then(bot, chatbot, chatbot, queue=True)
     submit.click(
         # fn=lambda x, y: ("",) + user(x, y)[1:],  # clear msg
@@ -406,7 +406,7 @@ with gr.Blocks(
         queue=True,
         # queue=False,
         show_progress="full",
-        api_name=None,
+        # api_name=None,
     ).then(bot, chatbot, chatbot, queue=True)
 
     clear.click(lambda: None, None, chatbot, queue=False)
